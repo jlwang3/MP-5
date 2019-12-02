ca131e41e8ffea84948bf1acaa3f6f419ca71a20
Rafi Gil Diaz <rfgildiaz@gmail.com>
2019-12-02 16:47:06 -0600

    end game

---
+++ app/src/main/java/com/example/mp_5/EndGameActivity.java
@@ -0,0 +1,4 @@
+package com.example.mp_5;
+
+public class EndGameActivity {
+}
--- app/src/main/java/com/example/mp_5/GameActivity.java
+++ app/src/main/java/com/example/mp_5/GameActivity.java
@@ -10,24 +10,49 @@ import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GameActivity extends AppCompatActivity {
+    private int score;
+
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_activity);
         for (int i = 0; i < 10; i++) {
             LinearLayout tiles = findViewById(R.id.tiles);
             final View messageChunk = getLayoutInflater().inflate(R.layout.layout, tiles, false);
-            String random = "button" + Integer.toString((int) ((Math.random() * (3)) + 1));
-            Button button = messageChunk.findViewById(R.id.random);
-            final MediaPlayer mp = MediaPlayer.create(this, R.raw.sample);
-            button.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
-            button.setOnClickListener(unused -> {
-                messageChunk.setVisibility(0);
-                mp.start();
+//            int random = (int) ((Math.random() * (3)) + 1);
+//            if (random == 1) {
+//                Button
+//            }
+            Button button1 = messageChunk.findViewById(R.id.button1);
+//            final MediaPlayer mp = MediaPlayer.create(this, R.raw.sample);
+            button1.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+//            button.setOnClickListener(unused -> {
+//                messageChunk.setVisibility(View.GONE);
+//                mp.start();
+//            });
+            Button button2 = messageChunk.findViewById(R.id.button2);
+            Button button3 = messageChunk.findViewById(R.id.button3);
+            Button button4 = messageChunk.findViewById(R.id.button4);
+
+            button1.setOnClickListener(unused -> {
+                messageChunk.setVisibility(View.GONE);
+                score++;
+            });
+
+            button2.setOnClickListener(unused -> {
+                endGame();
+            });
+
+            button3.setOnClickListener(unused -> {
+                endGame();
+            });
+
+            button4.setOnClickListener(unused -> {
+                endGame();
             });
         }
     }
     public void endGame() {
-        startActivity(new Intent(this, NewGameActivity.class));
+        startActivity(new Intent(this, EndGameActivity.class).putExtra("score", score));
         finish();
     }
 }
